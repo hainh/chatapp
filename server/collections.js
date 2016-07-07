27c08b4bb85b42cfd23446000b17deaffef32b43
hainh <hainh.vn@gmail.com>
2016-07-08 01:18:54 +0700

    v0.1

--- server/collections.js
+++ server/collections.js
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 
 var database = new MongoInternals.RemoteCollectionDriver('mongodb://chatapp:123qweASD@ds027328.mlab.com:27328/chatapp');
 
-let ChatMessages = new Mongo.Collection('messages', { _driver : database });
+let ChatMessages = new Mongo.Collection('messages', { _driver : database, capped: true, size: 50000000, max: 500 });
 
 Meteor.publish("messages", function(skip, limit) {
     console.log(skip + "   " + limit);
