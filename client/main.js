f56f32df0d0cce82c51648a7520446661c5e59a6
Hai Nguyen <hainh.vn@gmail.com>
2016-07-14 01:34:12 +0700

    + new version

--- client/main.js
+++ client/main.js
@@ -11,35 +11,70 @@ Meteor.subscribe("messages", 0, 5);
 
 import './main.html';
 
-Template.message.onCreated(function helloOnCreated() {
-
-});
 
 Template.messages.helpers({
   messages() {
     return ChatMessages.find({}, { sort : { time : 1 } });
   },
+
+  isMe(sender) {
+    console.log("sender = " + sender);
+        return sender == document.getElementById("username").value;
+  },
+
 });
 
-Template.message.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+function getTime0(time) {
+    var options;
+    var now = new Date();
+    console.log(now.toLocaleDateString() , time.toLocaleDateString());
+    if (now.toLocaleDateString() == time.toLocaleDateString()){
+        options = { hour: "2-digit", minute: "2-digit", hour12: false };
+    } else {
+        options = { year: '2-digit', month: '2-digit', day: '2-digit', hour: "2-digit", minute: "2-digit", hour12: false };
+    }
+
+    return time.toLocaleString("en-US", options);
+}
+
+Template.messageLeft.helpers({
+
+    getTime(time) {
+        return getTime0(time);
+    }
 });
+
+Template.messageRight.helpers({
+
+    getTime(time) {
+        return getTime0(time);
+    }
+});
+
+function scrollBottom() {
+    var dcm = $("#all-chat-messages");
+    dcm.scrollTop(dcm.prop("scrollHeight"));
+}
+
+Template.messageLeft.rendered = scrollBottom;
+Template.messageRight.rendered = scrollBottom;
 
 window.sendMessage = function sendMessage(e) {
-  console.log(e.keyCode);
-  if (e.keyCode !== 13) {
-    return;
-  }
-  var sender = document.getElementById("username").value;
-  var message = document.getElementById("message").value;
-  console.log(sender, message);
+    console.log(e.keyCode);
+    if (e.keyCode !== 13) {
+        return;
+    }
+    var sender = document.getElementById("username").value;
+    var message = document.getElementById("message").value;
+    console.log(sender, message);
 
-  if (sender.length < 3 || message < 2) {
-    return;
-  }
+    if (sender.length < 3 || message < 2) {
+        return;
+    }
 
-  ChatMessages.insert({time: new Date(), sender: sender, message : message });
+    localStorage.username = sender;
+
+    document.getElementById("message").value = "";
+
+    ChatMessages.insert({time: new Date(), sender: sender, message : message });
 }
